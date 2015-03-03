2a0f3e473f1b6d72ae79725a1e6705fcb1d81705
David Luz <davidmelo8@hotmail.com>
2015-03-03 11:07:03 -0300

    Commit 2
    Fixed drag and drop

--- js/game.js
+++ js/game.js
@@ -1,39 +1,55 @@
-//Atividade1 é um objeto
-Atividade1 = {};
+var game = new Phaser.Game(650, 400, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update});
 
-//States da Atividade
-Atividade1.MainMenu = function(){ }; 
-Atividade1.Play = function(){ }; 
-Atividade1.Relatorio = function(){ }; 
+function preload() {
 
-
-var game = new Phaser.Game(1280, 800, Phaser.CANVAS, 'game', { preload:preload, create: create, update: update });
-
-game.state.add('MainMenu',Atividade1.MainMenu);
-function preload(){
-   game.load.image('logo', 'imgs/logo.png');
-   game.load.image('btn_init', 'imgs/btn_init.png');
+    game.load.image('drag1', 'imgs/drag1.png');
+    game.load.image('drag2', 'imgs/drag2.png');
 
 }
+
+var drag1;
+var drag2;
 
 function create() {
 
+    game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    this.game.stage.backgroundColor = '#ffffff';
-    game.add.sprite(0, 0, 'logo');
-    var btn_init = game.add.sprite(0,0, 'btn_init');
-	btn_init.inputEnabled = true;
-	btn_init.events.onInputDown.add(goToPlay, this);
+    game.stage.backgroundColor = '#2d2d2d';
+
+    //  This will check Sprite vs. Sprite collision
+
+    drag1 = game.add.sprite(50, 200, 'drag1');
+    drag2 = game.add.sprite(0, 0, 'drag2');
+
+    game.physics.enable( [ drag1, drag2 ], Phaser.Physics.ARCADE);
+
+	
+    drag1.inputEnabled = true;
+    drag1.input.enableDrag(true);
+	drag1.bringToTop();
+	
+	drag2.inputEnabled = true;
+    drag2.input.enableDrag(true);
+	drag2.bringToTop();
+	
+
 
 }
 
-function update(){
+function update() {
 
-} 
+    // Usar overlap do arcade e não o collider
+    game.physics.arcade.overlap(drag1, drag2, collisionHandler);
 
-function goToPlay(){
-game.state.start('Play');
 }
+
+function collisionHandler (obj1, obj2) {
+
+    //  The two sprites are colliding
+    game.stage.backgroundColor = '#992d2d';
+
+}
+
 
 
 
