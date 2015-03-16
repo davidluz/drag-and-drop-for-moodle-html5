d442e1625851199f390eb6e4fa85ec3379e7e0be
David Luz <davidmelo8@hotmail.com>
2015-03-16 14:23:10 -0300

    V.3.0.1

--- js/game.js
+++ js/game.js
@@ -21,17 +21,21 @@ function preload(){
 
 game.load.image('bg', 'imgs/bg.png');
 game.load.image('home', 'imgs/home.png');
+game.load.image('home2', 'imgs/home2.png');
 game.load.image('comecar', 'imgs/comecar.png');
 
 }
 
 function create(){
 bg = game.add.sprite(0, 0, 'bg');
-home = game.add.sprite(200, 150, 'home');
+home = game.add.sprite(380, 180, 'home');
+home2 = game.add.sprite(210, 130, 'home2');
 comecar = game.add.sprite(300, 330, 'comecar');
 comecar.inputEnabled = true;
 comecar.events.onInputDown.add(goToPlay, this);
 game.stage.backgroundColor = '#fff';
+
+	 tween = game.add.tween(home).to({ x: 250 }, 4000, Phaser.Easing.Linear.None, true, 0, 1000, true);
 }
 
 function update(){
